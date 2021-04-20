5f205414f97726433bfde466062edaedcb737d66
mayu14 <shirakawa.mayu.0810@gmail.com>
2021-04-20 17:03:47 +0900

    Add: implement the method of reading YAML

--- fig2tex.py
+++ fig2tex.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 import argparse
+from pathlib import Path
+
+import yaml
+
 
 def get_cmd_args():
     parser = argparse.ArgumentParser("make tex source from some figure and YAML file")
@@ -9,7 +13,22 @@ def get_cmd_args():
     parser.add_argument('-n', '--number_of_fig', type=int, help='number of figure (assume start with 001~)',
                         required=True)
     parser.add_argument('-w', '--width', default='130', help='width of figure')
+    parser.add_argument('-c', '--captions_tag', default='captions', help='caption key of the yaml')
     return parser.parse_args()
+
+
+def get_captions(fname_yaml, captions_tag='captions'):
+    fname_yaml = Path(fname_yaml)
+    if not fname_yaml.exists():
+        raise FileNotFoundError(f"{fname_yaml} is not exist")
+
+    with open(fname_yaml) as f:
+        yml = yaml.safe_load(f)
+
+    if not captions_tag in yml:
+        raise KeyError(f"no key {captions_tag} in yaml file")
+
+    return yml[captions_tag]
 
 
 def get_figure_source(fig_name, caption, width=130):
@@ -23,7 +42,7 @@ def get_figure_source(fig_name, caption, width=130):
 
 def main():
     setting = get_cmd_args()
-
+    get_captions(setting.filename_YAML, setting.captions_tag)
 
 if __name__ == '__main__':
     main()
